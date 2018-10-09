4643b696495b8ca50efe9ca394e06fbee2bf06d1
ORBITAXBD\nafis.rahman <nafis.rahman@orbitax.com>
2018-10-09 13:44:19 +0600

    First Commit
    uploading all the files

---
+++ MyLearningPractice/src/DiffVariables.java
@@ -0,0 +1,32 @@
+
+public class DiffVariables {
+
+	public static void main(String[] args) {
+		
+		double a = 12.234;
+		
+		short b = 12345;
+		
+		int c = 'G';
+		
+		char d = 'D';
+		
+		boolean Y = true;
+		
+		long L = 1234243534;
+		
+		System.out.println(a);
+		
+		System.out.println(b);
+		
+		System.out.println(c);
+		
+		System.out.println(d);
+		
+		System.out.println(Y);
+		
+		System.out.println(L);
+
+	}
+
+}
